9dac5411e59c84fbdb5c05d458c7aa74a663c7f1
gabriel <gabriel@uma.es>
2009-06-12 14:35:02 +0000

    Escapadas las tildes del catalán. De ' a \'.
    git-svn-id: https://opengnsys.es/svn/trunk@67 a21b9725-9963-47de-94b9-378ad31fedc9

--- Hidra/webhidra/idiomas/php/cat/executarprocediments_cat.php
+++ Hidra/webhidra/idiomas/php/cat/executarprocediments_cat.php
@@ -6,9 +6,9 @@
 //________________________________________________________________________________________________________
 	$TbMsg=array();
 	$TbMsg[0]='Centres';
-	$TbMsg[1]='Grup d'aules';
+	$TbMsg[1]='Grup d\'aules';
 	$TbMsg[2]='Aules';
-	$TbMsg[3]='Grup d'ordinadors';
+	$TbMsg[3]='Grup d\'ordinadors';
 	$TbMsg[4]='Ordinadors';
 	$TbMsg[5]='Execució de procediments';
 	$TbMsg[6]='Ámbit';
