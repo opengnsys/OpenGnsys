fd33f2fc66b7d61124eb4fa7de5453c973b50e56
gabriel <gabriel@uma.es>
2009-06-12 14:34:25 +0000

    Escapadas las tildes del catalán. De ' a \'.
    git-svn-id: https://opengnsys.es/svn/trunk@66 a21b9725-9963-47de-94b9-378ad31fedc9

--- Hidra/webhidra/idiomas/php/cat/aules_cat.php
+++ Hidra/webhidra/idiomas/php/cat/aules_cat.php
@@ -5,15 +5,15 @@
 //	Idioma: Català 
 //________________________________________________________________________________________________________
 	$TbMsg=array();
-	$TbMsg[0]='"Nou grup d'aules"';
+	$TbMsg[0]='"Nou grup d\'aules"';
 	$TbMsg[1]='"Estatus ordinadors"';
-	$TbMsg[2]='"Purgar';
+	$TbMsg[2]='"Purgar"';
 	$TbMsg[3]='"Afegir nova aula"';
-	$TbMsg[4]='"Refrescar';
+	$TbMsg[4]='"Refrescar"';
 	$TbMsg[5]='"Ordres"';
-	$TbMsg[6]='"Cua d'accions"';
-	$TbMsg[7]='"Eliminar grup d'aules"';
-	$TbMsg[8]='"Nou grup d'ordinadors"';
+	$TbMsg[6]='"Cua d\'accions"';
+	$TbMsg[7]='"Eliminar grup d\'aules"';
+	$TbMsg[8]='"Nou grup d\'ordinadors"';
 	$TbMsg[9]='"Afegir nou ordinador"';
 	$TbMsg[10]='"Configuració"';
 	$TbMsg[11]='"Col.locar ordinador"';
@@ -21,20 +21,20 @@
 	$TbMsg[13]='"Propietats"';
 	$TbMsg[14]='"Eliminar aula"';
 	$TbMsg[15]='"Renombrar"';
-	$TbMsg[16]='"Eliminar grup d'ordinadors"';
+	$TbMsg[16]='"Eliminar grup d\'ordinadors"';
 	$TbMsg[17]='"Moure ordinador"';
 	$TbMsg[18]='"Eliminar ordinador"';
 	$TbMsg[19]='"Tasques"';
 	$TbMsg[20]='"Treballs"';
 	$TbMsg[21]='"Totes"';
 	$TbMsg[22]='Estatus ordinadors';
-	$TbMsg[23]='Aula d'ordinadors';
+	$TbMsg[23]='Aula d\'ordinadors';
 	$TbMsg[24]='Ámbit: Centres';
-	$TbMsg[25]='Ámbit: Grup d'aules';
-	$TbMsg[26]='Ámbito: Grup d'ordinadors';
+	$TbMsg[25]='Ámbit: Grup d\'aules';
+	$TbMsg[26]='Ámbito: Grup d\'ordinadors';
 	$TbMsg[27]='"Incorporar ordinadores"';
 	$TbMsg[28]='"Procedimentss"';
-	$TbMsg[29]='"Reserves d'aules"';
+	$TbMsg[29]='"Reserves d\'aules"';
 	$TbMsg[30]='"Confirmadas"';
 	$TbMsg[31]='"Pendents"';
 	$TbMsg[32]='"Denegades"';
