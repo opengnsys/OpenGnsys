383fe4bc3533031617c3719c78e213b4f6f84675
adelcastillo <adelcastillo@us.es>
2009-09-09 12:32:56 +0000

    Cambios en la estructura del cliente con initrd. Borrado codigo antiguo del navegador.
    git-svn-id: https://opengnsys.es/svn/trunk@186 a21b9725-9963-47de-94b9-378ad31fedc9

--- opengnsys-client/client/browser/browser.pro
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------------
-# Project created by QtCreator 2009-07-07T15:18:57
-# -------------------------------------------------
-QT += webkit
-TARGET = browser
-TEMPLATE = app
-SOURCES += main.cpp \
-    mainwindow.cpp
-HEADERS += mainwindow.h
---
+++ opengnsys-client/nfsexport/etc/preinit/default.sh
@@ -0,0 +1,19 @@
+#!/opt/opengnsys/bin/bash
+
+. /opt/opengnsys/lib/engine/bin/Install.lib
+
+ogLoadEnviron
+
+if [ -f $OGETC/init/$OG_IP.sh ]; then
+    $OGETC/init/$OG_IP.sh
+
+elif [ -f $OGETC/init/$OGGROUP.sh ]; then
+    $OGETC/init/$OGGROUP.sh
+
+elif [ -f $OGETC/init/default.sh ]; then
+    $OGETC/init/default.sh
+
+else
+    echo "No se ha encontrado script de inicio"
+    halt
+fi
