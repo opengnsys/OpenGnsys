139053ad573c9586e6fc70ca73bab8d601156e8d
adelcastillo <adelcastillo@us.es>
2009-09-10 02:56:29 +0000

    Anadido un archivo de configuracion del DHCP. Terminando el instalador del cliente initrd.
    git-svn-id: https://opengnsys.es/svn/trunk@189 a21b9725-9963-47de-94b9-378ad31fedc9

--- opengnsys-installer/client_initrd_installer.sh
+++ opengnsys-installer/client_initrd_installer.sh
@@ -41,43 +41,52 @@ function arguments_parser
     echo $SVNROOT
 }
 
+function install_necesary_packages
+{
+    apt-get install pxe dhcp3-server tftpd-hpa pxe nfs-kernel-server
+}
+
 function create_file_system
 {
-    mkdir -p $OGROOT
-    cp -rf $SVNROOT/opengnsys-client/nfsexports/ $OGROOT/client
-
     mkdir -p $TFTPBOOT
+
+    mkdir -p $OGROOT
+
+    mkdir -p $OGROOT/bin
+    mkdir -p $OGROOT/client
+    mkdir -p $OGROOT/lib
+    mkdir -p $OGROOT/images
+
     mkdir -p /etc/opengnsys/
     mkdir -p /var/log/opengnsys/clients/
     ln -s $TFTPBOOT $OGROOT/tftpboot/
     ln -s /etc/opengnsys/ /opt/opengnsys/etc/
     ln -s /var/log/opengnsys/ $OGROOT/log/
 
-    mkdir -p $OGROOT/bin
-    mkdir -p $OGROOT/lib
-    mkdir -p $OGROOT/images
+    install -d $SVNROOT/opengnsys-client/nfsexports/ $OGROOT/client
+    install -d $SVNROOT/opengnsys-client/engine/*.lib $OGROOT/client/lib/engine/
+}
+
+function dhcpd
+{
+    cat $SVNROOT/opengnsys-server/DHCP/dhcpd.conf >> /etc/dhcp3/dhcpd.conf
+    echo "Revise el archivo /etc/dhcp3/dhcpd.conf para configurarlo para su red"
 }
 
 function tftpboot
 {
-    cd $OGROOT/tftpboot/
-
-    if [ $? = 1 ]; then
-        mkdir -p pxelinux.cfg/
-        cd pxelinux.cfg
-        echo "default install" > default
-        echo "label install" >> default
-        echo "    kernel linux" >> default
-        echo "    append vga=788 initrd=initrd.gz acpi=on" >> default
-        cd ..
-
-        $SVNROOT/opengnsys-client/boot/initrd-generator
-    fi
+    mkdir -p $OGROOT/tftpboot/pxelinux.cfg/
+    cd $OGROOT/tftpboot/pxelinux.cfg/
+    cat $SVNROOT/opengnsys-server/PXE/pxelinux.cfg/default >> default
+    $SVNROOT/opengnsys-client/boot/initrd-generator
 }
 
 function nfsexport
 {
-    sleep 1
+    cat $SVNROOT/opengnsys-server/NFS/export >> /etc/exports
+    /etc/init.d/nfs-kernel-server restart
+
+    echo "Revise el archivo /etc/exports para configurarlo para su red"
 }
 
 arguments_parser $@
