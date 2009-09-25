28a505ef07233c5dcfa555e169ae4d80d91141cc
adv <adv@uma.es>
2009-09-25 10:27:03 +0000

    Se inserta la clase ToolsGNU para refenciar con doxygen y para el instalador las herramientas gnu utilizadas por opengnsys
    git-svn-id: https://opengnsys.es/svn/trunk@292 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ opengnsys-client/engine/ToolsGNU.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#/**
+#@file    ToolsGNU.c
+#@brief   Librería o clase Tools GNU used by OpenGNSys
+#@class   Tools
+#@brief   Herramientas gnu utilizadas por opengnsys.
+#@version 0.9
+#@warning License: GNU GPLv3+
+#*/
+
+
+function awk () {
+}
