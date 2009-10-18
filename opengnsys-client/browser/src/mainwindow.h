b88103b2c217bef0d2b7507383c17b879850f092
adelcastillo <adelcastillo@us.es>
2009-10-18 17:47:48 +0000

    Cambiado el sistema de compilacion de qmake a cmake. Anadido qtermwidget compilado con qtembedded 4.5.1 para agregarlo al browser.
    git-svn-id: https://opengnsys.es/svn/trunk@388 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ opengnsys-client/browser/src/mainwindow.h
@@ -0,0 +1,65 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#define PROTOCOL "command:"
+#define ENVIRONMENT "OGLOGFILE"
+//#define ENVIRONMENT "OGIP,OGSERVER,OGLOG"
+
+#include <QWidget>
+#include <QProcess>
+#include <QMap>
+#include <QMainWindow>
+
+class QWebView;
+class QTextEdit;
+class QVBoxLayout;
+class QProcess;
+class QStringList;
+class QString;
+class QUrl;
+class QFile;
+class QTextStream;
+class QDockWidget;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    public:
+        MainWindow(QWidget *parent = 0);
+        ~MainWindow();
+
+    public slots:
+        // Funcion que maneja los links
+        void slotLinkHandle(const QUrl& url);
+        void slotWebLoadStarted();
+        void slotWebLoadFinished(bool ok);
+        void slotWebLoadProgress(int progress);
+
+        // Funciones que manejan cada vez que el proceso hace algo
+        void slotProcessStarted();
+        void slotProcessFinished(int code,QProcess::ExitStatus status);
+        void slotProcessError(QProcess::ProcessError error);
+        void slotProcessOutput();
+        void slotProcessErrorOutput();
+
+    //Functions
+    protected:
+        int readEnvironmentValues();
+
+
+    protected:
+        QWebView *web;
+        QTextEdit *text;
+        QDockWidget *dock;
+
+        QProcess *process;
+        QStringList output;
+        QStringList errors;
+
+        QMap<QString,QString> env;
+        QFile *logfile;
+        QTextStream *logstream;
+};
+
+#endif // MAINWINDOW_H
