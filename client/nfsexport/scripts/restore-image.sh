e42f34ed09b02a78a78a4525d282aa456f37db6f
ramon <ramongomez@us.es>
2009-10-19 12:48:35 +0000

    Función ogCopyFile.
    git-svn-id: https://opengnsys.es/svn/trunk@391 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/nfsexport/scripts/restore-image.sh
+++ client/nfsexport/scripts/restore-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# restore-image.sh - Scirpt de ejemplo para restaurar una imagen.
+# Scirpt de ejemplo para restaurar una imagen.
 # (puede usarse como base para el programa de restauración de imágenes usado por OpenGNSys Admin).
 
 PROG="$(basename $0)"
@@ -9,13 +9,24 @@ if [ $# -ne 4 ]; then
 fi
 
 # Procesar parámetros de entrada
-IMGFILE=$(ogGetPath "$1" "$2.img") || exit $?
+if [ "$1" == "CACHE" -o "$1" == "cache" ]; then
+    # Si la imagen no está en la caché, copiarla del repositorio.
+    IMGDIR=$(ogGetParentPath "$1" "$2") || exit $?
+    IMGFILE=$(ogGetPath "$1" "$2")
+    if [ -z "$IMGFILE" ]; then
+        echo "Copiando imagen \"$2\" del repositorio a caché local"
+        ogCopyFile "repo" "$2" "$IMGDIR" || exit $?
+        IMGFILE=$(ogGetPath "cache" "$2") || exit $?
+    fi
+else
+    IMGFILE=$(ogGetPath "$1" "$2") || exit $?
+fi
 PART=$(ogDiskToDev "$3" "$4") || exit $?
-# Mostrar información.
-ogEcho info "$PROG: Origen=$PART, Destino=$IMGFILE"
 
+# Mostrar información.
+ogEcho info "$PROG: Origen=$IMGFILE, Destino=$PART"
 # Restaurar la imagen.
-ogRestoreImage "$@" || exit $?
+ogRestoreImage "$1" "$2" "$3" "$4" || exit $?
 # Restaurar tamaño.
 ogExtendFs $1 $2
 
