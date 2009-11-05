156182e8c58407844b44dfd9a35e90cbefa29989
alonso <alonso@us.es>
2009-11-05 15:01:01 +0000

    primeros archivos de administración
    git-svn-id: https://opengnsys.es/svn/trunk@506 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ admin/WebConsole/includes/ctrlacc.php
@@ -0,0 +1,40 @@
+<?
+// *************************************************************************************************************************************************
+// Aplicación WEB: ogAdmWebCon
+// Autor: José Manuel Alonso (E.T.S.I.I.) Universidad de Sevilla
+// Fecha Creación: Diciembre-2003
+// Fecha Última modificación: Febrero-2005
+// Nombre del fichero: controlacceso.php
+// Descripción :Este fichero implementa el control de acceso a la Aplicación en todas las p�inas
+// *************************************************************************************************************************************************
+session_start(); // Activa variables de sesi�
+$idcentro="";
+$nombrecentro="";
+$usuario="";
+$idtipousuario=0;
+$idioma="";
+$cadenaconexion="";
+$servidorhidra="";
+$hidraport="";
+$pagerror="";
+$urlacceso="";
+
+if (isset($_SESSION["widcentro"]))	$idcentro = $_SESSION["widcentro"];
+if (isset($_SESSION["wnombrecentro"]))	$nombrecentro = $_SESSION["wnombrecentro"];
+if (isset($_SESSION["wusuario"]))	$usuario = $_SESSION["wusuario"];
+if (isset($_SESSION["widtipousuario"]))	$idtipousuario = $_SESSION["widtipousuario"];
+if (isset($_SESSION["widioma"]))	$idioma = $_SESSION["widioma"];
+if (isset($_SESSION["wcadenaconexion"]))	$cadenaconexion = $_SESSION["wcadenaconexion"];
+if (isset($_SESSION["wservidorhidra"]))	$servidorhidra = $_SESSION["wservidorhidra"];
+if (isset($_SESSION["whidraport"]))	$hidraport = $_SESSION["whidraport"];
+if (isset($_SESSION["wpagerror"]))	$pagerror = $_SESSION["wpagerror"];
+if (isset($_SESSION["wurlacceso"]))	$urlacceso = $_SESSION["wurlacceso"];
+
+// Comprueba el valor de las variables de sesión�
+$swacc=empty($cadenaconexion) || empty($servidorhidra) || empty($hidraport) || empty($usuario) || empty($idtipousuario);
+//===============================================================================================
+if ($swacc){ // Error en alguna variable de sesión
+	die("Error de acceso");
+}
+//===============================================================================================
+?>
