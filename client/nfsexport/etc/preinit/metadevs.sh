0bc3a64abfebdffc0435a627ab8702e68064a219
ramon <ramongomez@us.es>
2010-04-19 12:32:53 +0000

    Detectar RAID en arranque del cliente.
    git-svn-id: https://opengnsys.es/svn/trunk@865 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/nfsexport/etc/preinit/metadevs.sh
+++ client/nfsexport/etc/preinit/metadevs.sh
@@ -2,12 +2,14 @@
 #/**
 #@file    metadevs.sh
 #@brief   Script de inicio para detectar metadispositivos LVM y RAID.
-#@todo    Pendiente detección de RAID.
 #@note    Desglose del script "loadenviron.sh".
 #@warning License: GNU GPLv3+
 #@version 0.9
 #@author  Ramon Gomez, ETSII Universidad de Sevilla
 #@date    2009-10-10
+#@version 0.9.4
+#@author  Ramon Gomez, ETSII Universidad de Sevilla
+#@date    2010-04-19
 #*/
 
 
@@ -16,7 +18,8 @@ if [ -n "$OPENGNSYS" ]; then
     echo "$MSG_DETECTLVMRAID"
     # Detectar metadispositivos LVM.
     vgchange -ay >/dev/null 2>&1
-    # FIXME Detectar metadispositivos RAID.
+    # Detectar metadispositivos RAID.
+    dmraid -ay >/dev/null 2>&1
 else
     # FIXME Error: entorno de OpenGNSys no configurado.
     echo "Error: OpenGNSys environment is not configured."   # FIXME: definir mensaje.
