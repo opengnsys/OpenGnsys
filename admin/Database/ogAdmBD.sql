05cfc624db3f0649bc7e055e08731642acc56ff7
adv <adv@uma.es>
2011-02-23 19:59:50 +0000

    version 1.0 --- bd -- ticket:302
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@1487 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/Database/ogAdmBD.sql
+++ admin/Database/ogAdmBD.sql
@@ -104,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `aulas` (
   `netmask` VARCHAR( 30 ),
   `dns` VARCHAR (30),
   `modp2p` enum('seeder','peer','leecher') DEFAULT 'seeder',
-  `timep2p` INT(11) NOT NULL DEFAULT '60'
+  `timep2p` INT(11) NOT NULL DEFAULT '60',
   PRIMARY KEY (`idaula`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
