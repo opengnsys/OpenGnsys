43f7ae80db163e6b8513d997b4b38bacc04aa9ff
ramon <ramongomez@us.es>
2011-03-22 09:32:45 +0000

    Terminal del Browser del cliente con letra de espaciado fijo (versiones^C.10 y 1.0).
    git-svn-id: https://opengnsys.es/svn/trunk@1599 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/browser/src/mainwindow.cpp
+++ client/browser/src/mainwindow.cpp
@@ -295,7 +295,7 @@ void MainWindow::slotCreateTerminal()
 {
     QTermWidget* console = new QTermWidget(1,this);
     QFont font = QApplication::font();
-    font.setFamily("Terminus");
+    font.setFamily("Courier");
     font.setPointSize(12);
     
     console->setTerminalFont(font);
