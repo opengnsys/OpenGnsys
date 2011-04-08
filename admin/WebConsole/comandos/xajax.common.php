fe86970a00c5913b0706ddf66c8ee461315f89ec
adv <adv@uma.es>
2011-04-08 13:04:06 +0000

    version1.0 eliminandos pruebas de asistentes dentro de estructura comandos #359
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@1752 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/comandos/xajax.common.php
+++
@@ -1,7 +0,0 @@
-<?php
-//importando las librerias XAJAX
-require ("/opt/opengnsys/www/xajax/xajax_core/xajax.inc.php");
-$xajax = new xajax("xajax.server.php");
-//asociamos la función creada en index.server.php al objeto XAJAX
-$xajax->registerFunction("ListarParticionesXip");
-?>
