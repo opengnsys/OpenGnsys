4adfd0aed607002ddd13948dd23a458d47eb2b8b
adv <adv@uma.es>
2011-04-24 17:23:25 +0000

    trunk #381 control de errores para el modulo www asistente de clonaciones remotas
    git-svn-id: https://opengnsys.es/svn/trunk@1889 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/asistentes/includes/asistentes/formCloneRemotePartition.php
+++ admin/WebConsole/asistentes/includes/asistentes/formCloneRemotePartition.php
@@ -10,8 +10,22 @@
 		</select>		
 		<br />		
 		<!--DIV donde se mostrara la respuesta AJAX sobre las particiones clonables del equipo-->
-		Elige particion del Master a enviar
+		Elige desde el Master la imagen o particion a enviar
 		<div id="divListado"></div>
+		<br />
+		Elige la identificacion de la partición destino de los clientes:
+		<select name="targetpart" id="targetpart" style="WIDTH:220";">
+			<option value="1 1"> 1er disco - 1ª particion </option>
+			<option value="1 2"> 1er disco - 2ª particion </option>
+			<option value="1 3"> 1er disco - 3ª particion </option>
+			<option value="1 4"> 1er disco - 4ª particion </option>
+		</select>
+		<br />
+		Elige el metodo de transferencia
+		<select name="idmetodo" id="idmetodo" style="WIDTH:220";">
+			<option value="MULTICAST"> MULTICAST </option>
+			<option value="UNICAST"> UNICAST </option> 			
+		</select>
 		<br />
 		Elige herramienta de clonacion:
 		<select name="tool" id="tool" style="WIDTH:220";">
@@ -24,15 +38,9 @@
 				<option value="lzop"> lzop </option>
 				<option value="gzip"> gzip </option>
 		</select>
-			<br />		
-	
+		<br />		
 		<br />
-		Elige el metodo de transferencia
-		<select name="idmetodo" id="idmetodo" style="WIDTH:220";">
-			<option value="MULTICAST"> MULTICAST </option>
-			<option value="UNICAST"> UNICAST </option> 			
-		</select>
-		<br />
+
 	</td>
 	
 	<td class="op_mcast">
