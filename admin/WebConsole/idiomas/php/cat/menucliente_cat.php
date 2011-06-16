375ee701eb26faa0efd748bcc2624fa6e828ff5a
ramon <ramongomez@us.es>
2011-06-16 11:11:14 +0000

    Corregir erratas en menú de cliente por defecto (modifica #413)
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2068 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/idiomas/php/cat/menucliente_cat.php
+++ admin/WebConsole/idiomas/php/cat/menucliente_cat.php
@@ -5,8 +5,8 @@
 //	Idioma: Español 
 //________________________________________________________________________________________________________
 	$TbMsg=array();
-	$TbMsg[0]='"***ATENCI&Oacute;.- No està accedint des d'un ordinador permès"';
+	$TbMsg[0]='"***ATENCI&Oacute;.- No està accedint des d\'un ordinador permès"';
 	$TbMsg[1]='"Direcci&oacute; IP"';
-	$TbMsg[2]='"NO S'HA DETECTAT CAP MEN&Uacute; PER AQUEST CLIENT"';
-	$TbMsg[3]='"Apagar l'equip"';
+	$TbMsg[2]='NO S\'HA DETECTAT CAP MEN&Uacute; PER AQUEST CLIENT';
+	$TbMsg[3]='Apagar l\'equip';
 ?>
--- admin/WebConsole/idiomas/php/esp/menucliente_esp.php
+++ admin/WebConsole/idiomas/php/esp/menucliente_esp.php
@@ -7,6 +7,6 @@
 	$TbMsg=array();
 	$TbMsg[0]='"***ATENCI&Oacute;N.- Usted no est&aacute; accediendo desde un ordenador permitido"';
 	$TbMsg[1]='"Direcci&oacute;n IP"';
-	$TbMsg[2]='"NO SE HA DETECTADO NING&Uacute;N MEN&Uacute; PARA ESTE CLIENTE"';
-	$TbMsg[3]='"Apagar el equipo"';
+	$TbMsg[2]='NO SE HA DETECTADO NING&Uacute;N MEN&Uacute; PARA ESTE CLIENTE';
+	$TbMsg[3]='Apagar el equipo';
 ?>
