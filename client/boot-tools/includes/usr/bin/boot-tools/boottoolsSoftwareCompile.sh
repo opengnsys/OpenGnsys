ab29875f4167554f746f9acaf89f2e6bf936954b
adv <adv@uma.es>
2011-08-02 17:21:08 +0000

    version 1.0.2 #404 #420
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2254 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+################################################################
+##################### SOFTWARE #####################
+################################################################
+export LANGUAGE=C
+export LC_ALL=C
+export LANG=C
+
+#dpkg-divert --local --rename --add /sbin/initctl 
+#ln -s /bin/true /sbin/initctl
+
+apt-get -y update
+# software to compile code
+apt-get -y --force-yes install build-essential libattr* attr make m4 gettext libmhash-dev gdebi-core gawk
+
+source /opt/opengnsys/lib/engine/bin/ToolsGNU.c
+
+#TODO: comprobar si esta instalado.
+ctorrent install
+
+#TODO: comprobar si esta instalado.
+udpcast install
+
+#ntfs-3g install
+
+#TODO: comprobar si esta instalado.
+ms-sys install
+
+#TODO: comprobar si esta instalado.
+wget -O partclone_0.2.16_i386.deb http://downloads.sourceforge.net/project/partclone/stable/0.2.16/partclone_0.2.16_i386.deb?use_mirror=ovh
+gdebi -n partclone_0.2.16_i386.deb 
+
+#TODO: comprobar si esta instalado.
+cd /tmp
+wget http://damien.guibouret.free.fr/savepart.zip
+unzip savepart.zip -d /sbin/
+
+
+#apt-get remove build-essential libattr* attr make m4 gettext libmhash-dev gdebi-core gawk
+
