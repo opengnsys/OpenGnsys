3d00dd9b3f0a032df36110927804fd757898130e
adv <adv@uma.es>
2011-08-03 15:23:57 +0000

    version 1.0.2 #420 compatible con OG v2 etc/init/default
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2259 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/shared/etc/init/default.sh
+++ client/shared/etc/init/default.sh
@@ -9,13 +9,13 @@ PASS=$(grep "^[ 	]*OPTIONS=" /scripts/ogfunctions 2>&1 | \
 PASS=${PASS:-"og"}
 echo -ne "$PASS\n$PASS\n" | passwd root 2>/dev/null
 
-#inicio del servidor sshd
+# Inicio del servidor sshd
 /usr/sbin/sshd
 
-#desactivando apagado de monitor
+# Desactivado apagado de monitor
 #setterm -blank 0 -powersave off -powerdown 0 < /dev/console > /dev/console 2>&1
 
-#activando WOL en la interfaz usada en arranque pxe
+# Activado WOL en la interfaz usada en arranque pxe
 ethtool -s $DEVICE wol g 2>/dev/null
 
 # Fichero de registro de incidencias (en el servidor; si no, en local).
@@ -26,16 +26,22 @@ if ! touch $OGLOGFILE 2>/dev/null; then
 fi
 LOGLEVEL=5
 
-#facilitando el entorno Og desde ssh
+#Facilitando el entorno Og desde ssh
 cp $OPENGNSYS/etc/preinit/loadenviron.sh /etc/profile.d/
 
 # Crear menú por defecto para el cliente
 generateMenuDefault
 
-#Matando plymount para inicir browser o shell
+# Matando plymount para inicir browser o shell
 pkill -9 plymouthd
 
-# Arranque de OpenGnSys Client.
+# Arranque de OpenGnSys Client daemon (web services).
+if [ -x $OPENGNSYS/job_executer/init.d/job_executer ]; then
+    echo "Running Opengnsys client daemon (web services)"
+    $OPENGNSYS/job_executer/init.d/job_executer restart
+fi
+
+# Arranque de OpenGnSys Client daemon (socket).
 if [ -x "$OPENGNSYS/bin/ogAdmClient" ]; then
     echo "$MSG_LAUNCHCLIENT"
     [ $ogactiveadmin == "true" ] && boot=admin
