41193048040aed1e756824a5890f3bd6388d65f1
adv <adv@uma.es>
2011-09-21 14:56:43 +0000

    branch version 1.0 #440 partclone 0.2.8
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2311 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
+++ client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
@@ -20,7 +20,8 @@ echo  "ms-sys "
 which ms-sys || ms-sys install &>/dev/null
 
 echo  "echo partclone "
-which partclone.ntfs || $(wget -O partclone_0.2.16_i386.deb http://downloads.sourceforge.net/project/partclone/stable/0.2.16/partclone_0.2.16_i386.deb?use_mirror=ovh &>/dev/null; gdebi -n partclone_0.2.16_i386.deb &>/dev/null) 
+#which partclone.ntfs || $(wget -O partclone_0.2.16_i386.deb http://downloads.sourceforge.net/project/partclone/stable/0.2.16/partclone_0.2.16_i386.deb?use_mirror=ovh &>/dev/null; gdebi -n partclone_0.2.16_i386.deb &>/dev/null) 
+which partclone.ntfs || gdebi -n /var/cache/apt/archivesOG/partclone_0.2.8_i386.deb
 
 echo  "spartlnx"
 which spartlnx.run || $(wget http://damien.guibouret.free.fr/savepart.zip &>/dev/null; unzip -o savepart.zip -d /sbin/)
