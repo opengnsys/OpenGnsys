95d4495adaf50e02342755a55ba1849475e19b88
adv <adv@uma.es>
2011-12-12 14:45:17 +0000

    instalacion paquetes deb #460
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2391 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
+++ client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
@@ -19,14 +19,23 @@ which udp-sender || udpcast install &>/dev/null
 echo  "ms-sys "
 which ms-sys || ms-sys install &>/dev/null
 
-echo  "echo partclone "
+#echo  "echo partclone "
 #which partclone.ntfs || $(wget -O partclone_0.2.16_i386.deb http://downloads.sourceforge.net/project/partclone/stable/0.2.16/partclone_0.2.16_i386.deb?use_mirror=ovh &>/dev/null; gdebi -n partclone_0.2.16_i386.deb &>/dev/null) 
-which partclone.ntfs || gdebi -n /var/cache/apt/archivesOG/partclone_0.2.8_i386.deb
+#which partclone.ntfs || gdebi -n /var/cache/apt/archivesOG/partclone_0.2.8_i386.deb
 
 echo  "spartlnx"
 which spartlnx.run || $(wget http://damien.guibouret.free.fr/savepart.zip &>/dev/null; unzip -o savepart.zip -d /sbin/)
 
 echo "xvesa"
 gdebi -n /var/cache/apt/archivesOG/xvesa.deb
+
+echo "partclone"
+gdebi -n /var/cache/apt/archivesOG/partclone_0.2.38_i386.deb
+
+echo "busybox"
+gdebi -n /var/cache/apt/archivesOG/busybox_1.17.1-10ubuntu1_i386.deb.deb
+
+echo "busybox"
+gdebi -n /var/cache/apt/archivesOG/busybox-static_1.17.1-10ubuntu1_i386.deb.deb
 
 history -c
