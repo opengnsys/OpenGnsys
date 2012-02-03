cd3ebe29040e83ec13a02220622028a0025b1c93
ramon <ramongomez@us.es>
2012-02-03 08:44:00 +0000

    Versión 1.0.3, #454: Integrar código de buscador de equipos en consola web.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2483 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ admin/WebConsole/idiomas/php/eng/buscar_eng.php
@@ -0,0 +1,18 @@
+<?
+//________________________________________________
+//
+//	Fichero de idiomas php: buscar_eng.php 
+//	Idioma: English
+//________________________________________________
+$TbMsg=array();
+$TbMsg["SEARCH_TITLE"]="SEARCH COMPUTERS";
+$TbMsg["SEARCH_CRITERIA"]="Search criteria";
+$TbMsg["SEARCH_RESULTS"]="Search results";
+$TbMsg["SEARCH_LAB"]="Lab.";
+$TbMsg["SEARCH_NAME"]="Hostname";
+$TbMsg["SEARCH_IP"]="IP address";
+$TbMsg["SEARCH_MAC"]="MAC address";
+$TbMsg["SEARCH_DUPLICATES"]="Duplicates";
+$TbMsg["SEARCH_NOMATCHES"]="There are no matches";
+?>
+
---
+++ admin/WebConsole/idiomas/php/esp/buscar_esp.php
@@ -0,0 +1,18 @@
+<?
+//________________________________________________
+//
+//	Fichero de idiomas php: buscar_esp.php 
+//	Idioma: Español 
+//________________________________________________
+$TbMsg=array();
+$TbMsg["SEARCH_TITLE"]="BUSCAR EQUIPOS";
+$TbMsg["SEARCH_CRITERIA"]="Criterio de b&uacute;squeda";
+$TbMsg["SEARCH_RESULTS"]="Resultados de la b&uacute;squeda";
+$TbMsg["SEARCH_LAB"]="Aula";
+$TbMsg["SEARCH_NAME"]="Nombre";
+$TbMsg["SEARCH_IP"]="Direcci&oacute;n IP";
+$TbMsg["SEARCH_MAC"]="Direcci&oacute;n MAC";
+$TbMsg["SEARCH_DUPLICATES"]="Duplicados";
+$TbMsg["SEARCH_NOMATCHES"]="No se encuentran resultados";
+?>
+
