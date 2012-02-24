4f6896ab9156ba81e7d3e26ba12e3ff9e527dbed
ramon <ramongomez@us.es>
2012-02-24 17:42:00 +0000

    Versión 1.0.3, #483: Corregir algunos nombres de ficheros de idiomas.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@2686 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/idiomas/javascripts/eng/comandos/inventariosoftware_eng.js
+++ admin/WebConsole/idiomas/javascripts/eng/comandos/inventariosoftware_eng.js
@@ -1,8 +1,8 @@
 //________________________________________________________________________________________________________
 //
-//	Fichero de idiomas javascripts: iniciarsesion_esp.js 
-//	Idioma: Español 
+//	Fichero de idiomas javascripts: iniciarsesion_eng.js 
+//	Idioma: English
 //________________________________________________________________________________________________________
 TbMsg=new Array;
 TbMsg[0]="";
-TbMsg[1]="Debe elegir al menos una partición";
+TbMsg[1]="You must choose at least one partition";
