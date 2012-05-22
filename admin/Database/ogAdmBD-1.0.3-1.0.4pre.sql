77648b6fcd3a79f53380b737f82190f709f7d9ef
ramon <ramongomez@us.es>
2012-05-22 09:52:01 +0000

    Versión 1.0.4, #526: Nuevos códigos GPT y corrección de errata en actualización de la BD.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@3068 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/Database/ogAdmBD-1.0.3-1.0.4pre.sql
+++ admin/Database/ogAdmBD-1.0.3-1.0.4pre.sql
@@ -11,22 +11,27 @@ INSERT INTO ogAdmBD.tipospar (codpar,tipopar,clonable) VALUES
 	(6, 'FAT16', 1),
 	(CONV('A5',16,10), 'FREEBSD', 1),
 	(CONV('A6',16,10), 'OPENBSD', 1),
-	(CONV('AF',16,10), 'HFS', 1);
+	(CONV('AF',16,10), 'HFS', 1),
 	(CONV('BE',16,10), 'SOLARIS-BOOT', 1),
 	(CONV('DA',16,10), 'DATA', 1),
 	(CONV('EE',16,10), 'GPT', 0),
 	(CONV('EF',16,10), 'EFI', 0),
 	(CONV('FB',16,10), 'VMFS', 1),
 	(CONV('0700',16,10), 'WINDOWS', 1),
+	(CONV('0C01',16,10), 'WIN-RESERV', 1),
+	(CONV('7F00',16,10), 'CHROMEOS-KRN', 1),
+	(CONV('7F01',16,10), 'CHROMEOS', 1),
+	(CONV('7F02',16,10), 'CHROMEOS-RESERV', 1),
 	(CONV('8200',16,10), 'LINUX-SWAP', 0),
 	(CONV('8300',16,10), 'LINUX', 1),
-	(CONV('8301',16,10), 'CACHE', 0),
+	(CONV('8301',16,10), 'LINUX-RESERV', 1),
 	(CONV('8E00',16,10), 'LINUX-LVM', 1),
 	(CONV('A500',16,10), 'FREEBSD-DISK', 0),
 	(CONV('A501',16,10), 'FREEBSD-BOOT', 1),
 	(CONV('A502',16,10), 'FREEBSD-SWAP', 0),
 	(CONV('A503',16,10), 'FREEBSD', 1),
 	(CONV('AF00',16,10), 'HFS', 1),
+	(CONV('AF01',16,10), 'HFS-RAID', 1),
 	(CONV('BE00',16,10), 'SOLARIS-BOOT', 1),
 	(CONV('BF00',16,10), 'SOLARIS', 1),
 	(CONV('BF01',16,10), 'SOLARIS', 1),
@@ -34,6 +39,7 @@ INSERT INTO ogAdmBD.tipospar (codpar,tipopar,clonable) VALUES
 	(CONV('BF03',16,10), 'SOLARIS-DISK', 1),
 	(CONV('BF04',16,10), 'SOLARIS', 1),
 	(CONV('BF05',16,10), 'SOLARIS', 1),
+	(CONV('CA00',16,10), 'CACHE', 0),
 	(CONV('EF00',16,10), 'EFI', 0),
 	(CONV('EF01',16,10), 'MBR', 0),
 	(CONV('EF02',16,10), 'BIOS-BOOT', 0),
@@ -41,3 +47,4 @@ INSERT INTO ogAdmBD.tipospar (codpar,tipopar,clonable) VALUES
 	(CONV('FFFF',16,10), 'UNKNOWN', 1);
 
 ALTER TABLE ogAdmBD.ordenadores ADD fotoord VARCHAR (250) NOT NULL;
+
