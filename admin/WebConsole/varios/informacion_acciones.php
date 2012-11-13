631f14e4a9a97f8fceb541523fa7b940aa30e902
ramon <ramongomez@us.es>
2012-11-13 13:27:11 +0000

    Versión 1.0.5, #567: Completar código para corregir ejecución duplicada de asistentes.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@3408 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/varios/informacion_acciones.php
+++ admin/WebConsole/varios/informacion_acciones.php
@@ -190,6 +190,7 @@ function SubarbolXML_comandos($descricomando,$parametros,$visuparametros)
 			if(isset($tbParametrosValor[$nemo])){
 				for($j=0;$j<sizeof($tbParametrosValor[$nemo])-1;$j++){
 					$descripcion=$tbParametrosValor[$nemo]["descripcion"];
+					$valor=$tbParametrosValor[$nemo][$j]["valor"];
 					if(sizeof($tbParametrosValor[$nemo])>2)
 						$valor=$tbParametrosValor[$nemo][$j]["valor"];
 					else
