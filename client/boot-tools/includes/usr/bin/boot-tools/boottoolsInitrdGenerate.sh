24549d44f4aa7db439c7e862b03665bab4bb4e7f
ramon <ramongomez@us.es>
2013-01-22 13:31:19 +0000

    #573: Usar fichero de configuración para generar el Initrd del cliente.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@3490 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/boot-tools/includes/usr/bin/boot-tools/boottoolsInitrdGenerate.sh
+++ client/boot-tools/includes/usr/bin/boot-tools/boottoolsInitrdGenerate.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-export OSDISTRIB=$(lsb_release -i | awk -F: '{sub(/\t/,""); print $2}') 2>/dev/null
-#OSCODENAME=$(lsb_release -c | awk -F: '{sub(/\t/,""); print $2}') 2>/dev/null
-export OSCODENAME=$(cat /etc/lsb-release | grep CODENAME | awk -F= '{print $NF}')
-export OSRELEASE=$(uname -a | awk '{print $3}')
-uname -a | grep x86_64 > /dev/null  &&  export OSARCH=amd64 || export OSARCH=i386
-export OSHTTP="http://es.archive.ubuntu.com/ubuntu/"
 
+OGCLIENTCFG=${OGCLIENTCFG:-/tmp/ogclient.cfg}
+[ -f $OGCLIENTCFG ] && source $OGCLIENTCFG
+OSRELEASE=${OSRELEASE:-$(uname -a | awk '{print $3}')}
 
 
 cd /usr/lib/initramfs-tools/bin/
@@ -16,3 +13,4 @@ mkinitramfs -o /tmp/initrd.img-$OSRELEASE -v $OSRELEASE
 cp /boot/vmlinuz-$OSRELEASE /tmp
 
 history -c
+
