8c3eaa9e532991e4552c4cb018ee6447859c10b0
ramon <ramongomez@us.es>
2013-03-04 13:35:31 +0000

    #245 #573 #574: Boot-tools para nuevo cliente ogLive incluye Browser con soporte HTTPS y teclado internacional, pero no añade soporte para la rama discontinuada de OG2.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@3656 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/boot-tools/includes/usr/bin/boot-tools/boottoolsFsOpengnsys.sh
+++ client/boot-tools/includes/usr/bin/boot-tools/boottoolsFsOpengnsys.sh
@@ -17,7 +17,6 @@ NAMEHOSTCLIENTFILE="/tmp/opengnsys_chroot"
 SVNCLIENTDIR=/tmp/opengnsys_installer/opengnsys/client/boot-tools
 SVNCLIENTSTRUCTURE=/tmp/opengnsys_installer/opengnsys/client/shared
 SVNCLIENTENGINE=/tmp/opengnsys_installer/opengnsys/client/engine
-SVNOG2=/tmp/opengnsys_installer/opengnsys2
  
 OGCLIENTMOUNT=""
 
@@ -63,55 +62,30 @@ then
 	exit 1
 fi
 
-# copiamos algunas cosas del nfsexport
 
-#### Tipos de letra para el Browser.
-cp -a ${SVNCLIENTSTRUCTURE}/lib/fonts $OGCLIENTMOUNT/usr/local/lib/fonts
-#### Crear enlaces para compatibilidad con las distintas versiones del Browser.
-mkdir -p $OGCLIENTMOUNT/usr/local/Trolltech/QtEmbedded-4.5.1/lib/
-mkdir -p $OGCLIENTMOUNT/usr/local/QtEmbedded-4.6.2/lib/
-mkdir -p $OGCLIENTMOUNT/usr/local/QtEmbedded-4.6.3/lib/
-cp -a ${SVNCLIENTSTRUCTURE}/lib/fonts $OGCLIENTMOUNT/usr/local/Trolltech/QtEmbedded-4.5.1/lib/fonts 
-cp -a ${SVNCLIENTSTRUCTURE}/lib/fonts $OGCLIENTMOUNT/usr/local/QtEmbedded-4.6.2/lib/fonts 
-cp -a ${SVNCLIENTSTRUCTURE}/lib/fonts $OGCLIENTMOUNT/usr/local/QtEmbedded-4.6.3/lib/fonts
-if [ $? -ne 0 ]
-then 
-	echo "$FUNCNAME(): Linking Browser fonts : ERROR"
-	exit 1
-fi
+# Si no existe, copiar pci.ids.
+[ -f $OGCLIENTMOUNT/etc/pci.ids ] || cp -va ${SVNCLIENTSTRUCTURE}/lib/pci.ids $OGCLIENTMOUNT/etc
 
-#########################################################
-cp -a ${SVNCLIENTSTRUCTURE}/lib/pci.ids $OGCLIENTMOUNT/etc
-if [ $? -ne 0 ]
-then 
-	echo "$FUNCNAME(): Copying pci.ids : ERROR"
-	exit 1
-fi
-####### Browsser
+# Dependencias Qt para el Browser.
+mkdir -p $OGCLIENTMOUNT/usr/local/{etc,lib,plugins}
+cp -av ${SVNCLIENTSTRUCTURE}/lib/qtlib/* $OGCLIENTMOUNT/usr/local/lib
+cp -av ${SVNCLIENTSTRUCTURE}/lib/fonts $OGCLIENTMOUNT/usr/local/lib
+cp -av ${SVNCLIENTSTRUCTURE}/lib/qtplugins/* $OGCLIENTMOUNT/usr/local/plugins
+cp -av ${SVNCLIENTSTRUCTURE}/etc/*.qmap $OGCLIENTMOUNT/usr/local/etc
+
+# Browser.
 cp -av ${SVNCLIENTSTRUCTURE}/bin/browser $OGCLIENTMOUNT/bin
-if [ $? -ne 0 ]
-then 
+if [ $? -ne 0 ]; then 
 	echo "$FUNCNAME(): Copying Browser : ERROR"
 	exit 1
 fi
 
-
-#Compatiblidad con og2
-cp -av ${SVNCLIENTSTRUCTURE}/bin/browser2 $OGCLIENTMOUNT/bin
-cp -av ${SVNOG2}/ogr/ogr $OGCLIENTMOUNT/opt/opengnsys/bin/
-cp -av ${SVNOG2}/ogr/libogr.py $OGCLIENTMOUNT/usr/lib/python2.7/libogr.py
-cp -av ${SVNOG2}/ogr/libogr.py $OGCLIENTMOUNT/usr/lib/python2.6/libogr.py
-cp -av ${SVNOG2}/ogr/libogr.py $OGCLIENTMOUNT/opt/opengnsys/lib/python
-
-echo "mkdir -p /opt/opengnsys/lib/engine/"
-mkdir -p /opt/opengnsys/engine/
-echo "cp -prv ${SVNOG2}/engine/2.0/* $OGCLIENTMOUNT/opt/opengnsys/engine/" 
-cp -av ${SVNOG2}/engine/2.0/* $OGCLIENTMOUNT/opt/opengnsys/engine/
-
-cp -av ${SVNOG2}/job_executer $OGCLIENTMOUNT/opt/opengnsys/bin/
-
-
-cp -av ${SVNCLIENTSTRUCTURE}/bin/ogAdmClient  $OGCLIENTMOUNT/bin
+# ogAdmClient.
+cp -av ${SVNCLIENTSTRUCTURE}/bin/ogAdmClient $OGCLIENTMOUNT/bin
+if [ $? -ne 0 ]; then 
+	echo "$FUNCNAME(): Copying ogAdmClient: ERROR"
+	exit 1
+fi
 
 # El fichero de configuración debe sustituir a los 2 ficheros (borrar las 2 líneas).
 echo "${VERSIONBOOTTOOLS}-${OSCODENAME}-${OSRELEASE}-${VERSIONSVN}" > /$NAMEISOCLIENTFILE
