20876b108d65be891d06ef252cf80b733763f11b
albertogp <albertogp@uma.es>
2013-06-24 07:28:37 +0000

    branches-version1.0 #610 Validación Nombre Imagen -Integrando ticket en Versión 1.0 - Añadiendo idiomas
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@3936 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/idiomas/php/esp/propiedades_imagenes_esp.php
+++ admin/WebConsole/idiomas/php/esp/propiedades_imagenes_esp.php
@@ -16,12 +16,13 @@ $TbMsg[7]="Comentarios";
 $TbMsg[8]="Partición";
 $TbMsg[9]="Tipo partición";
 $TbMsg[10]="Repositorio";
-$TbMsg[11]="Nombre";
+$TbMsg[11]="Nombre Canónico";
 $TbMsg[12]="Gestión Imágenes Básicas";
 $TbMsg[13]="Gestión Imágenes Incrementales";
 $TbMsg[14]="Imagen básica";
 $TbMsg[15]="ATENCIÓN.- Una imagen que tiene perfil software indica que en algún momento se ha creado <br>
 			y por tanto no pueden modificarse ciertos datos a menos que la elimine y la vuelva a crear";
 $TbMsg[16]="Ruta Origen";
-
+$TbMsg[17]='"Nombre canónico fichero-imagen existe : Introduzca otro nombre"';
+$TbMsg[18]="Introduzca otro Nombre";
 ?>
