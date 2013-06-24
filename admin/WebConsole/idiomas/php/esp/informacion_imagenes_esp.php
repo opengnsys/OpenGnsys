5ba4240bcf6fa3618c8e2cc62f3cb10cc3bc7621
albertogp <albertogp@uma.es>
2013-06-24 07:51:24 +0000

    branches-version1.0 #610 Validación Nombre Imagen -Integrando ticket en Versión 1.0
    Añadiendo variables originales ya que en la integración del ticket
    se ha cometido un error sobre el fichero informacion_imagenes_esp.php
    
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@3937 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/idiomas/php/esp/informacion_imagenes_esp.php
+++ admin/WebConsole/idiomas/php/esp/informacion_imagenes_esp.php
@@ -1,28 +1,18 @@
 <?
 //________________________________________________________________________________________________________
 //
-//	Fichero de idiomas php: propiedades_imagenes_esp.php 
+//	Fichero de idiomas php: informacion_imagenes_esp.php 
 //	Idioma: Español 
 //________________________________________________________________________________________________________
-$TbMsg=array();
-$TbMsg[0]="Sin Opción";
-$TbMsg[1]="Insertar";
-$TbMsg[2]="Modificar";
-$TbMsg[3]="Eliminar";
-$TbMsg[4]="Gestión Imágenes Monolíticas";
-$TbMsg[5]="Descripción";
-$TbMsg[6]="Perfil Software";
-$TbMsg[7]="Comentarios";
-$TbMsg[8]="Partición";
-$TbMsg[9]="Tipo partición";
-$TbMsg[10]="Repositorio";
-$TbMsg[11]="Nombre Canónico";
-$TbMsg[12]="Gestión Imágenes Básicas";
-$TbMsg[13]="Gestión Imágenes Incrementales";
-$TbMsg[14]="Imagen básica";
-$TbMsg[15]="ATENCIÓN.- Una imagen que tiene perfil software indica que en algún momento se ha creado <br>
-			y por tanto no pueden modificarse ciertos datos a menos que la elimine y la vuelva a crear";
-$TbMsg[16]="Ruta Origen";
-$TbMsg[17]='"Nombre canónico fichero-imagen existe : Introduzca otro nombre"';
-$TbMsg[18]="Introduzca otro Nombre";
+$TbMsg=array(); 
+$TbMsg[0]='Imágenes'; 
+$TbMsg[1]='Information sobre imagenes'; 
+$TbMsg[2]='Imagen'; 
+$TbMsg[3]='Información de la Imagen'; 
+$TbMsg[4]='Componentes software'; 
+$TbMsg[5]='Software incremental'; 
+$TbMsg[6]='Perfil Software'; 
+$TbMsg[7]='Ordenadores con esta imagen'; 
+$TbMsg[8]='Partición'; 
+$TbMsg[9]='Comentarios'; 
 ?>
