380e3e0ea1fcafee15131c82466600b9be425830
albertogp <albertogp@uma.es>
2013-07-22 11:23:03 +0000

    branches-version1.0 Ajuste de cajas de información a 1024X768 en fichero httpd-log.sh
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@4013 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/shared/lib/httpd/httpd-log.sh
+++ client/shared/lib/httpd/httpd-log.sh
@@ -7,10 +7,10 @@ echo "<html><head><title>OpenGnsys Client</title></head><body>"
 
 echo "<h1> $(./httpd-runengine.sh 'ogEcho $MSG_HTTPLOG_NOUSE'  ) .    host $(hostname -s)</h1> "
 
-echo "<IFRAME SRC='bandwidth.sh' WIDTH=250 HEIGHT=80> <A HREF="bandwidth.sh">link</A> </IFRAME> "
+echo "<IFRAME SRC='bandwidth.sh' WIDTH=250 HEIGHT=90> <A HREF="bandwidth.sh">link</A> </IFRAME> "
 echo "<IFRAME SRC='cache.sh'     WIDTH=590 HEIGHT=90><A HREF="cache.sh">link</a> </IFRAME><br>"
 
 echo "<IFRAME SRC='LogSession.sh' WIDTH=850 HEIGHT=230> <A HREF="LogSession.sh">link</A> </IFRAME>"
-echo "<IFRAME SRC='LogCommand.sh' WIDTH=850 HEIGHT=250> <A HREF="LogCommand.sh">link</A> </IFRAME>"
+echo "<IFRAME SRC='LogCommand.sh' WIDTH=850 HEIGHT=280> <A HREF="LogCommand.sh">link</A> </IFRAME>"
 
 echo "</body>
