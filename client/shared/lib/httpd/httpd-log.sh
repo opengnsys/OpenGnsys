540e2a8cd769083a347bc2697cb6ac705d0b9350
albertogp <albertogp@uma.es>
2013-07-22 11:23:49 +0000

    branches-version1.0 Ajuste de cajas de información a 1024X768 en fichero httpd-log.sh
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@4014 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/shared/lib/httpd/httpd-log.sh
+++ client/shared/lib/httpd/httpd-log.sh
@@ -5,7 +5,7 @@ echo ""
 #echo "<html><head><meta http-equiv='Refresh' content='2,URL=./example3.sh'><title>Bash as CGI"
 echo "<html><head><title>OpenGnsys Client</title></head><body>"
 
-echo "<h1> $(./httpd-runengine.sh 'ogEcho $MSG_HTTPLOG_NOUSE'  ) .    host $(hostname -s)</h1> "
+echo "<h1> $(./httpd-runengine.sh 'ogEcho $MSG_HTTPLOG_NOUSE'  ) .</br>host $(hostname -s)</h1> "
 
 echo "<IFRAME SRC='bandwidth.sh' WIDTH=250 HEIGHT=90> <A HREF="bandwidth.sh">link</A> </IFRAME> "
 echo "<IFRAME SRC='cache.sh'     WIDTH=590 HEIGHT=90><A HREF="cache.sh">link</a> </IFRAME><br>"
