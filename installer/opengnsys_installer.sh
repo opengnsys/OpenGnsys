de1220c2c20b435baee5cce5a53aa364c2c5bfd0
ramon <ramongomez@us.es>
2014-02-24 11:52:17 +0000

    #616: Instalar usando ogLive 1.0.4.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@4156 a21b9725-9963-47de-94b9-378ad31fedc9

--- installer/opengnsys_installer.sh
+++ installer/opengnsys_installer.sh
@@ -1374,8 +1374,8 @@ function copyClientFiles()
 function clientCreate()
 {
 	local DOWNLOADURL="http://$OPENGNSYS_SERVER/downloads"
-	#local FILENAME=ogLive-precise-3.2.0-23-generic-r3257.iso	# 1.0.4-rc2
-	local FILENAME=ogLive-raring-3.8.0-22-generic-r3836.iso		# 1.0.5-rc3
+	local FILENAME=ogLive-precise-3.2.0-23-generic-r3257.iso	# 1.0.4-rc2
+	#local FILENAME=ogLive-raring-3.8.0-22-generic-r3836.iso	# 1.0.5-rc3
 	local TARGETFILE=$INSTALL_TARGET/lib/$FILENAME
 	local TMPDIR=/tmp/${FILENAME%.iso}
  
