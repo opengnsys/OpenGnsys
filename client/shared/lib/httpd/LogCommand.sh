30ad471609a53482410d51b66f0c9f764c2e944a
irina <irinagomez@us.es>
2014-03-04 13:07:35 +0000

    #565 #636 revisión mensajes de error en las sincronizadas.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@4173 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/shared/lib/httpd/LogCommand.sh
+++ client/shared/lib/httpd/LogCommand.sh
@@ -8,7 +8,7 @@ echo "<meta charset='utf-8'>"
 echo "<meta http-equiv='Refresh' content='5,URL=./LogCommand.sh'> <link rel='stylesheet' type='text/css' href='oglive.css' />"
 echo "<title>Bash as CGI</title></head><body>"
 
-$(strings -e S $OGLOGCOMMAND > $OGLOGCOMMANDTMP)
+strings -e S $OGLOGCOMMAND > $OGLOGCOMMANDTMP
 
 echo "<table>"
 
@@ -18,7 +18,7 @@ echo "<tr>"
 echo "<TEXTAREA NAME='trackloghead' ROWS='13' COLS='175'>"
 #echo "$(head -n 10 /tmp/command.log.tmp | uniq)"
 # UHU - 2013/07/05 - Se incluye el simbolo % y la palabra sent para que se muestre la salida de rsync
-echo "$(egrep -v '^bytes|%|sent|^Elapsed:|^Total [Tt]ime:|^-|^\\|^\/' $OGLOGCOMMANDTMP | uniq | head -n 15)" 
+egrep -v '^aclfilter|^deleting|^bytes|%|sent|^Elapsed:|^Total [Tt]ime:|^-|^\\|^\/' $OGLOGCOMMANDTMP | uniq | head -n 15 
 echo "</TEXTAREA>"
 
 echo "</tr>"
@@ -29,7 +29,7 @@ echo "<tr>"
 echo "<TEXTAREA NAME='tracklogtail' ROWS='2' COLS='175'>"
 #echo "$(tail -n 5 /tmp/command.log.tmp | uniq)"
 # UHU - 2013/07/05 - Se incluye el simbolo % y la palabra sent para que se muestre la salida de rsync
-echo "$(egrep '^bytes|%|sent|^Elapsed:|^Total [Tt]ime:|^-|^\\|^\/' $OGLOGCOMMANDTMP | uniq | tail -n 2)" 
+egrep '^aclfilter|^deleting|^bytes|%|sent|^Elapsed:|^Total [Tt]ime:|^-|^\\|^\/' $OGLOGCOMMANDTMP | uniq | tail -n 2 
 echo "</TEXTAREA>"
 
 
