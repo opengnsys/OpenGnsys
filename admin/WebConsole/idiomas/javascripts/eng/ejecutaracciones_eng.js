b26a0a42f6e52a659a446f2917f4b7011762b439
ramon <ramongomez@us.es>
2014-07-14 08:24:12 +0000

    Incluir en rama {{{branches/version1.0}}} los ficheros de idiomas que faltaban y que sí están en {{{trunk}}}.
    git-svn-id: https://opengnsys.es/svn/branches/version1.0@4348 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ admin/WebConsole/idiomas/javascripts/eng/ejecutaracciones_eng.js
@@ -0,0 +1,10 @@
+﻿//____________________________________________________________________________
+//	
+//	Fichero de idiomas javascript: ejecutaracciones_eng.js 
+//	Idioma: Ingles
+//____________________________________________________________________________
+TbMsg=new Array;
+TbMsg[0]="ATENCIÓN.- El Procedimiento será ejecutado aplicándolo a este ámbito. ¿ Está seguro ?";
+TbMsg[1]="El Procedimiento ha sido gestionado correctamente para este ámbito";
+TbMsg[2]="ATENCIÓN.- El Procedimiento se convertirá en procedimiento de inicio (Autoexec) para todos los ordenadores de este ámbito. ¿ Está seguro ?";
+
---
+++ admin/WebConsole/idiomas/javascripts/eng/inclusionacciones_eng.js
@@ -0,0 +1,10 @@
+﻿//________________________________________________________________________________________________________
+//	
+//	Fichero de idiomas javascript: inclusionacciones_eng.js 
+//	Idioma: Ingles
+//________________________________________________________________________________________________________
+TbMsg=new Array;
+TbMsg[0]="";
+TbMsg[1]=" ATENCIÓN: Debe introducir un número de orden correcto";
+TbMsg[2]="El proceso se ha ejecutado correctamente";
+
