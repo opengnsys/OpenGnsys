bc875768083f78b79c6c74368225f8b1cb1d743c
irina <irinagomez@us.es>
2017-04-10 08:36:07 +0000

    #779 Consola web: Se incluye el comando 'Enviar mensaje' en los ficheros de idioma comando/mensaje_idioma.php.
    git-svn-id: https://opengnsys.es/svn/branches/version1.1@5260 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/idiomas/php/esp/comandos/mensajes_esp.php
+++ admin/WebConsole/idiomas/php/esp/comandos/mensajes_esp.php
@@ -11,6 +11,7 @@ $TbMsg["COMMAND_Configurar"]="Particionar y Formatear";
 $TbMsg["COMMAND_CrearImagen"]="Crear Imagen";
 $TbMsg["COMMAND_EjecutarScript"]="Ejecutar Script";
 $TbMsg["COMMAND_EliminarImagenCache"]="Eliminar Imagen Cache";
+$TbMsg["COMMAND_EnviarMensaje"]="Enviar Mensaje";
 $TbMsg["COMMAND_IniciarSesion"]="Iniciar Sesi&oacute;n";
 $TbMsg["COMMAND_InventarioHardware"]="Inventario Hardware";
 $TbMsg["COMMAND_InventarioSoftware"]="Inventario Software";
@@ -21,13 +22,6 @@ $TbMsg["COMMAND_CrearImagenBasica"]="Crear Imagen B&aacute;sica";
 $TbMsg["COMMAND_RestaurarImagenBasica"]="Restaurar Imagen B&aacute;sica";
 $TbMsg["COMMAND_CrearSoftIncremental"]="Crear Imagen Incremental";
 $TbMsg["COMMAND_RestaurarSoftIncremental"]="Restaurar Imagen Incremental";
-
-$TbMsg["COMMAND_CreateBaseImage"]="Crear Imagen B&aacute;sica";
-$TbMsg["COMMAND_RestoreBaseImage"]="Restaurar Imagen B&aacute;sica";
-$TbMsg["COMMAND_CreateDiffImage"]="Crear Imagen Diferencial";
-$TbMsg["COMMAND_RestoreDiffImage"]="Restaurar Imagen Diferencial";
-
-
 $TbMsg["WIZARD_Asistente Particionado"]="Asistente de particionado";
 $TbMsg["WIZARD_Asistente Clonacion Particiones Remotas"]="Clonar particiones remotas";
 $TbMsg["WIZARD_Asistente Deploy de Imagenes"]="Deploy de imagenes";
