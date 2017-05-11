7d21851baa49f230ca4add302bb5caa3bfbf0570
ramon <ramongomez@us.es>
2017-05-11 12:06:02 +0000

    #244 #746: Browser muestra un tooltip con el nombre del proyecto cuando el ratón se pone sobre el icono; subir binario y actualizar ficheros de idiomas.
    git-svn-id: https://opengnsys.es/svn/branches/version1.1@5294 a21b9725-9963-47de-94b9-378ad31fedc9

--- client/browser/src/mainwindow.cpp
+++ client/browser/src/mainwindow.cpp
@@ -94,6 +94,7 @@ MainWindow::MainWindow(QWidget *parent)
       m_logo->setPixmap(logo);
     else
       m_logo->setText("OG");
+    m_logo->setToolTip(tr(gettext("Proyecto OpenGnsys"))+"\nhttp://opengnsys.es");
     // Progress bar
     m_progressBar=new QProgressBar(this);
     m_progressBar->setRange(0,100);
