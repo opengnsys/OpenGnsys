8e952dae9a144899afcadb874f7d3b72eafc3cdc
ramon <ramongomez@us.es>
2018-01-10 10:51:46 +0000

    #730: Evitar división por 0 al mostrar tamaños de ficheros.
    git-svn-id: https://opengnsys.es/svn/branches/version1.1@5549 a21b9725-9963-47de-94b9-378ad31fedc9

--- admin/WebConsole/includes/comunes.php
+++ admin/WebConsole/includes/comunes.php
@@ -393,6 +393,10 @@
 		$si_prefix = array( 'B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB' );
 		$base = 1024;
 		$class = min((int)log($bytes , $base) , count($si_prefix) - 1);
-		return sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class];
+		if ($class < 0) {
+			return '0 B';
+		} else {
+			return sprintf('%1.2f' , $bytes / pow($base, $class)) . ' ' . $si_prefix[$class];
+		}
 	}
 
