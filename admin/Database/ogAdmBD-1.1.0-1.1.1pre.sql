01c5e57fdc1da93f9c0974ccb2dd267d3e366188
ramon <ramongomez@us.es>
2018-02-26 08:10:51 +0000

    Versión 1.1.1: Adaptar rama {{{branches/version1.1}}} para la nueva versión.
    git-svn-id: https://opengnsys.es/svn/branches/version1.1@5611 a21b9725-9963-47de-94b9-378ad31fedc9

---
+++ admin/Database/ogAdmBD-1.1.0-1.1.1pre.sql
@@ -0,0 +1,4 @@
+### Fichero de actualización de la base de datos.
+# OpenGnsys 1.1.0 - OpenGnsys 1.1.1
+#use ogAdmBD
+
---
+++ admin/Database/ogAdmBD-1.1.1pre.sql
@@ -0,0 +1,4 @@
+### Fichero de actualización de la base de datos.
+# OpenGnsys 1.1.1
+#use ogAdmBD
+
