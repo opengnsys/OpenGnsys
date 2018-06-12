2152313a11b13e852ce77e4d319fbdeb25b77cbf
Fredy <aluque@soleta.eu>
2018-06-12 13:38:33 +0200

    #837 devel update script

--- installer/pkg-generator/tmp/opengnsys_installer/opengnsys_git_update.sh
+++ installer/pkg-generator/tmp/opengnsys_installer/opengnsys_git_update.sh
@@ -487,11 +487,11 @@ function updateWebFiles()
 
 	# Copiar los ficheros nuevos conservando el archivo de configuración de acceso.
 	backupFile $INSTALL_TARGET/www/controlacceso.php
-	mv $INSTALL_TARGET/www $INSTALL_TARGET/WebConsole
-	rsync -irplt $WORKDIR/opengnsys/admin/WebConsole $INSTALL_TARGET
-	ERRCODE=$?
-	mv $INSTALL_TARGET/WebConsole $INSTALL_TARGET/www
-	unzip -o $WORKDIR/opengnsys/admin/xajax_0.5_standard.zip -d $INSTALL_TARGET/www/xajax
+	#~ mv $INSTALL_TARGET/www $INSTALL_TARGET/WebConsole
+	#~ rsync -irplt $WORKDIR/opengnsys/admin/WebConsole $INSTALL_TARGET
+	#~ ERRCODE=$?
+	#~ mv $INSTALL_TARGET/WebConsole $INSTALL_TARGET/www
+#	unzip -o $WORKDIR/opengnsys/admin/xajax_0.5_standard.zip -d $INSTALL_TARGET/www/xajax
 	unzip -o $WORKDIR/opengnsys/admin/slim-2.6.1.zip -d $INSTALL_TARGET/www/rest
 	unzip -o $WORKDIR/opengnsys/admin/swagger-ui-2.2.5.zip -d $INSTALL_TARGET/www/rest
 	if [ $ERRCODE != 0 ]; then
