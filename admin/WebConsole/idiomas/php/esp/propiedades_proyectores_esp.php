a167244a30d78400120a38465e8b8da957174ad8
Ramón M. Gómez <ramongomez@us.es>
2018-06-14 14:30:34 +0200

    #794: Projector properties form (initial version).

---
+++ admin/WebConsole/idiomas/php/esp/propiedades_proyectores_esp.php
@@ -0,0 +1,16 @@
+<?php
+//______________________________________________________________
+//
+//	Fichero de idiomas php: propiedades_proyectores_esp.php 
+//	Idioma: Español 
+//______________________________________________________________
+if (empty ($TbMsg)) $TbMsg=array();
+$TbMsg[0]="Sin Opci&oacute;n";
+$TbMsg[1]="Insertar";
+$TbMsg[2]="Modificar";
+$TbMsg[3]="Eliminar";
+$TbMsg[4]="Gestión Proyectores";
+$TbMsg["PROP_IPADDR"]="Direcci&oacute;n IP";
+$TbMsg["PROP_MODEL"]="Marca y Modelo";
+$TbMsg["PROP_NAME"]="Nombre";
+$TbMsg["PROP_TYPE"]="Tipo de Proyector";
