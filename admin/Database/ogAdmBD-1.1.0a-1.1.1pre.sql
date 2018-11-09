52a1490abc5bebe5acd8294539701e0a864dacf5
Ramón M. Gómez <ramongomez@us.es>
2018-11-09 10:58:26 +0100

    #872: Renaming some SQL files.

--- admin/Database/ogAdmBD-1.1.0a-1.1.1pre.sql
+++ admin/Database/ogAdmBD-1.1.0a-1.1.1pre.sql
@@ -1,5 +1,5 @@
 ### Fichero de actualización de la base de datos.
-# OpenGnsys 1.1.0 - OpenGnsys 1.1.1
+# OpenGnsys 1.1.0, 1.1.0a - OpenGnsys 1.1.1
 #use ogAdmBD
 
 # Añadir campo para incluir PC de profesor de aula (ticket #816).
