2adfccd0021093fb7019cee0b43c823a7140c032
Ramón M. Gómez <ramongomez@us.es>
2019-06-04 13:23:01 +0200

    #917: Fixed bug when compiling {{{ms-sys}}} command on ogLive.

--- client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
+++ client/boot-tools/includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
@@ -13,8 +13,8 @@ export LANG=C
 source /opt/opengnsys/lib/engine/bin/ToolsGNU.c &>/dev/null
 pushd /tmp
 
-echo  "ms-sys "
-which ms-sys || ms-sys install &>/dev/null
+echo  "ms-sys"
+which ms-sys || (wget https://sourceforge.net/projects/ms-sys/files/latest/download -O ms-sys.tar.gz; tar xpvzf ms-sys.tar.gz; cd $(tar tzf ms-sys.tar.gz|head -1); make install; cd ..)
 
 echo  "spartlnx"
 which spartlnx.run || $(wget http://damien.guibouret.free.fr/savepart.zip &>/dev/null; unzip -o savepart.zip -d /sbin/)
