59c16e0681227d84e039e6975194f0c4ea4446b8
Javier Sánchez Parra <jsanchez@soleta.eu>
2019-07-09 12:06:32 +0200

    #915 adds test for POST /hardware REST API
    This test covers two two scenarios:
    
    1. Correct usage.
    2. Incorrect usage, use this command with GET (this returns http 405).

---
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0011_post_hardware.py
@@ -0,0 +1,20 @@
+import requests
+import unittest
+
+class TestPostHardwareMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://localhost:8888/hardware'
+        self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
+        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ] }
+
+    def test_post(self):
+        returned = requests.post(self.url, headers=self.headers, json=self.json)
+        self.assertEqual(returned.status_code, 200)
+
+    def test_get(self):
+        returned = requests.get(self.url, headers=self.headers)
+        self.assertEqual(returned.status_code, 405)
+
+if __name__ == '__main__':
+    unittest.main()
