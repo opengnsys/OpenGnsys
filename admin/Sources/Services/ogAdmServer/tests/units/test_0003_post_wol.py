7af06a5f75330ca0c706103bd8cea9ee8c69fb93
Javier Sánchez Parra <jsanchez@soleta.eu>
2019-08-28 10:52:23 +0200

    #915 add missing brackets to WOL API test json
    Without the brackets the call works (200 response) but the ogAdmServer
    does not send the WOL packets.

--- admin/Sources/Services/ogAdmServer/tests/units/test_0003_post_wol.py
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0003_post_wol.py
@@ -6,8 +6,8 @@ class TestPostWolMethods(unittest.TestCase):
     def setUp(self):
         self.url = 'http://localhost:8888/wol'
         self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
-        self.json = { 'type' : 'broadcast', 'clients' : { 'addr' : '192.168.2.1',
-            'mac' : '00AABBCCDD01' } }
+        self.json = { 'type' : 'broadcast', 'clients' : [ { 'addr' : '192.168.2.1',
+            'mac' : '00AABBCCDD01' } ] }
 
     def test_post(self):
         returned = requests.post(self.url, headers=self.headers, json=self.json)
