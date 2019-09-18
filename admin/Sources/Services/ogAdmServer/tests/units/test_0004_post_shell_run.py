ad10ec16eb46fd50ad238038833e0675d01266de
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-09-18 12:59:16 +0200

    #915: Fix incorrect method and code in invalid GET requests
    Some tests should perform GET requests however they incorrectly use POST.
    Return codes for these tests have also been fixed.

--- admin/Sources/Services/ogAdmServer/tests/units/test_0004_post_shell_run.py
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0004_post_shell_run.py
@@ -13,8 +13,8 @@ class TestPostShellRunMethods(unittest.TestCase):
         self.assertEqual(returned.status_code, 200)
 
     def test_get(self):
-        returned = requests.post(self.url, headers=self.headers)
-        self.assertEqual(returned.status_code, 404)
+        returned = requests.get(self.url, headers=self.headers)
+        self.assertEqual(returned.status_code, 405)
 
 if __name__ == '__main__':
     unittest.main()
