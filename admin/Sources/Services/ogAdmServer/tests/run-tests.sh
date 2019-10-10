ca96a723d15d86ed5856b36d35b889724fa64528
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-10-10 13:10:07 +0200

    #915 Add test for POST "image/create/incremental" command
    This test covers correction of the command.

--- admin/Sources/Services/ogAdmServer/tests/run-tests.sh
+++ admin/Sources/Services/ogAdmServer/tests/run-tests.sh
@@ -16,4 +16,5 @@ curl -X POST -H "Authorization: $API_KEY" http://127.0.0.1:8888/image/create -d 
 curl -X POST -H "Authorization: $API_KEY" http://127.0.0.1:8888/image/restore -d @restore_image.json
 curl -X POST -H "Authorization: $API_KEY" http://127.0.0.1:8888/image/setup -d @setup_image.json
 curl -X POST -H "Authorization: $API_KEY" http://127.0.0.1:8888/image/create/basic -d @create_basic_image.json
+curl -X POST -H "Authorization: $API_KEY" http://127.0.0.1:8888/image/create/incremental -d @create_incremental_image.json
 curl -X POST -H "Authorization: $API_KEY" http://127.0.0.1:8888/image/restore/basic -d @restore_basic_image.json
