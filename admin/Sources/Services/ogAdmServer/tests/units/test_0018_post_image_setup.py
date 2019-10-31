2293c0e0874710ac712c50810877e0a708655234
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-10-31 11:46:00 +0100

    #915 Add test for POST /image/setup REST API
    This test covers 3 scenarios:
    
    1. Correct usage.
    2. Incorrect usage, without payload.
    3. Incorrect usage, use this command with GET.

---
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0018_post_image_setup.py
@@ -0,0 +1,47 @@
+import requests
+import unittest
+
+class TestPostSetupImageMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://localhost:8888/image/setup'
+        self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
+        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ],
+                      'disk' : '1',
+                      'cache' : '1',
+                      'cache_size' : '0',
+                      'partition_setup': [{'partition': '1',
+                                           'code': 'LINUX',
+                                           'filesystem': 'EMPTY',
+                                           'size': '498688',
+                                           'format': '0'},
+                                          {'partition': '2',
+                                           'code': 'LINUX-SWAP',
+                                           'filesystem': 'EMPTY',
+                                           'size': '199987',
+                                           'format': '0'},
+                                          {'partition': '3',
+                                           'code': 'LINUX',
+                                           'filesystem': 'EMPTY',
+                                           'size': '31053824',
+                                           'format': '0'},
+                                          {'partition': '4',
+                                           'code': 'EMPTY',
+                                           'filesystem': 'EMPTY',
+                                           'size': '0',
+                                           'format': '0'}] }
+
+    def test_post(self):
+        returned = requests.post(self.url, headers=self.headers, json=self.json)
+        self.assertEqual(returned.status_code, 200)
+
+    def test_no_payload(self):
+        returned = requests.post(self.url, headers=self.headers, json=None)
+        self.assertEqual(returned.status_code, 400)
+
+    def test_get(self):
+        returned = requests.get(self.url, headers=self.headers)
+        self.assertEqual(returned.status_code, 405)
+
+if __name__ == '__main__':
+    unittest.main()
