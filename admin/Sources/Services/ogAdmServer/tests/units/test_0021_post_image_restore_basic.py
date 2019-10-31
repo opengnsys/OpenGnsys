8303f9605476d8f348bab63d93493ba00ff0365a
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-10-31 11:50:59 +0100

    #915 Add test for POST /image/restore/basic REST API
    This test covers 3 scenarios:
    
    1. Correct usage.
    2. Incorrect usage, without payload.
    3. Incorrect usage, use this command with GET.

---
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0021_post_image_restore_basic.py
@@ -0,0 +1,41 @@
+import requests
+import unittest
+
+class TestPostRestoreBasicImageMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://localhost:8888/image/restore/basic'
+        self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
+        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ],
+                      'disk': '1',
+                      'partition': '1',
+                      'id': '9',
+                      'name': 'test',
+                      'repository': '192.168.56.10',
+                      'profile': '17',
+                      'type': 'UNICAST',
+                      'sync_params':{'path': '',
+                                     'method': '1',
+                                     'sync': '1',
+                                     'diff': '0',
+                                     'remove': '1',
+                                     'compress': '0',
+                                     'cleanup': '0',
+                                     'cache': '0',
+                                     'cleanup_cache': '0',
+                                     'remove_dst': '0'} }
+
+    def test_post(self):
+        returned = requests.post(self.url, headers=self.headers, json=self.json)
+        self.assertEqual(returned.status_code, 200)
+
+    def test_no_payload(self):
+        returned = requests.post(self.url, headers=self.headers, json=None)
+        self.assertEqual(returned.status_code, 400)
+
+    def test_get(self):
+        returned = requests.get(self.url, headers=self.headers)
+        self.assertEqual(returned.status_code, 405)
+
+if __name__ == '__main__':
+    unittest.main()
