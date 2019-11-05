d3bed4e91a49e3620bdc846e47954095f2e8a79e
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-11-05 10:26:44 +0100

    #915 Fix ogAdmServer POST /software test parameters
    This patch adds missing parameters to the test JSON.

--- admin/Sources/Services/ogAdmServer/tests/units/test_0012_post_software.py
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0012_post_software.py
@@ -6,7 +6,9 @@ class TestPostSoftwareMethods(unittest.TestCase):
     def setUp(self):
         self.url = 'http://localhost:8888/software'
         self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
-        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ] }
+        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ],
+                      'disk' : '0',
+                      'partition' : '1' }
 
     def test_post(self):
         returned = requests.post(self.url, headers=self.headers, json=self.json)
