64aef4d001923ec19f3fa0e93e025b7f771c160f
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-11-06 13:37:01 +0100

    #915 Validate POST /image/restore/incremental REST API parameters
    This patch ensures that all required parameters are sent in the request.

--- admin/Sources/Services/ogAdmServer/sources/ogAdmServer.cpp
+++ admin/Sources/Services/ogAdmServer/sources/ogAdmServer.cpp
@@ -4544,28 +4544,58 @@ static int og_cmd_restore_incremental_image(json_t *element, struct og_msg_param
 		return -1;
 
 	json_object_foreach(element, key, value) {
-		if (!strcmp(key, "clients"))
+		if (!strcmp(key, "clients")) {
 			err = og_json_parse_clients(value, params);
-		else if (!strcmp(key, "disk"))
+		} else if (!strcmp(key, "disk")) {
 			err = og_json_parse_string(value, &params->disk);
-		else if (!strcmp(key, "partition"))
+			params->flags |= OG_REST_PARAM_DISK;
+		} else if (!strcmp(key, "partition")) {
 			err = og_json_parse_string(value, &params->partition);
-		else if (!strcmp(key, "id"))
+			params->flags |= OG_REST_PARAM_PARTITION;
+		} else if (!strcmp(key, "id")) {
 			err = og_json_parse_string(value, &params->id);
-		else if (!strcmp(key, "name"))
+			params->flags |= OG_REST_PARAM_ID;
+		} else if (!strcmp(key, "name")) {
 			err = og_json_parse_string(value, &params->name);
-		else if (!strcmp(key, "repository"))
+			params->flags |= OG_REST_PARAM_NAME;
+		} else if (!strcmp(key, "repository")) {
 			err = og_json_parse_string(value, &params->repository);
-		else if (!strcmp(key, "profile"))
+			params->flags |= OG_REST_PARAM_REPO;
+		} else if (!strcmp(key, "profile")) {
 			err = og_json_parse_string(value, &params->profile);
-		else if (!strcmp(key, "type"))
+			params->flags |= OG_REST_PARAM_PROFILE;
+		} else if (!strcmp(key, "type")) {
 			err = og_json_parse_string(value, &params->type);
-		else if (!strcmp(key, "sync_params"))
-			err = og_json_parse_sync_params(value, &(params->sync_setup));
+			params->flags |= OG_REST_PARAM_TYPE;
+		} else if (!strcmp(key, "sync_params")) {
+			err = og_json_parse_sync_params(value, params);
+		}
 
 		if (err < 0)
 			break;
 	}
+
+	if (!og_msg_params_validate(params, OG_REST_PARAM_ADDR |
+					    OG_REST_PARAM_DISK |
+					    OG_REST_PARAM_PARTITION |
+					    OG_REST_PARAM_ID |
+					    OG_REST_PARAM_NAME |
+					    OG_REST_PARAM_REPO |
+					    OG_REST_PARAM_PROFILE |
+					    OG_REST_PARAM_TYPE |
+					    OG_REST_PARAM_SYNC_DIFF_ID |
+					    OG_REST_PARAM_SYNC_DIFF_NAME |
+					    OG_REST_PARAM_SYNC_PATH |
+					    OG_REST_PARAM_SYNC_METHOD |
+					    OG_REST_PARAM_SYNC_SYNC |
+					    OG_REST_PARAM_SYNC_DIFF |
+					    OG_REST_PARAM_SYNC_REMOVE |
+					    OG_REST_PARAM_SYNC_COMPRESS |
+					    OG_REST_PARAM_SYNC_CLEANUP |
+					    OG_REST_PARAM_SYNC_CACHE |
+					    OG_REST_PARAM_SYNC_CLEANUP_CACHE |
+					    OG_REST_PARAM_SYNC_REMOVE_DST))
+		return -1;
 
 	len = snprintf(buf, sizeof(buf),
 		       "nfn=RestaurarSoftIncremental\rdsk=%s\rpar=%s\ridi=%s\rnci=%s\r"
