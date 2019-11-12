6dc823e4d2f2378981cd84a59dcfcc241076bc20
Roberto Hueso Gómez <rhueso@soleta.eu>
2019-11-12 13:53:36 +0100

    #915 Adapt POST /shell/run test to the 'echo' parameter

--- admin/Sources/Services/ogAdmServer/tests/units/test_0004_post_shell_run.py
+++ admin/Sources/Services/ogAdmServer/tests/units/test_0004_post_shell_run.py
@@ -6,7 +6,9 @@ class TestPostShellRunMethods(unittest.TestCase):
     def setUp(self):
         self.url = 'http://localhost:8888/shell/run'
         self.headers = {'Authorization' : '07b3bfe728954619b58f0107ad73acc1'}
-        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ], 'run' : 'ls' }
+        self.json = { 'clients' : [ '192.168.2.1', '192.168.2.2' ],
+                      'run' : 'ls',
+                      'echo': True}
 
     def test_post(self):
         returned = requests.post(self.url, headers=self.headers, json=self.json)
