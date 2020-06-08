efbe3cae676d640ff6ad16fae527a23d421af2bc
Ramón M. Gómez <ramongomez@us.es>
2020-06-08 19:02:56 +0200

    #959: Update script checks all defined tags.

--- installer/opengnsys_update.sh
+++ installer/opengnsys_update.sh
@@ -197,7 +197,7 @@ function chooseVersion()
         # Fetch tags (releases) data from GitHub.
         while read -pe TAG URL; do
             if [[ $TAG =~ ^opengnsys- ]]; then
-                [ "${TAG#opengnsys-}" \< "${INSTVERSION%pre}" ] && break
+                [ "${TAG#opengnsys-}" \< "${INSTVERSION%pre}" ] && continue
                 RELDATE=$(curl -s "$URL" | jq -r '.commit.committer.date | split("-") | join("")[:8]')
                 RELEASES+=( "${TAG} ($RELDATE)" )
                 DOWNLOADS+=( "$URL" )
