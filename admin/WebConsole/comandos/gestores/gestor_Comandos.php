opengnsys/OpenGnsys

commit afbfe2ad7df10b3d90555dee1089030b6bfd25b5

#1032 Use create_schedule_now for immediate logged commands
Avoid stale commands from being executed when given a time schedule that
include past dates, ie. when they are manually scheduled.

Use create_schedule_now for immediate execution commands (no manual
schedule) that the user want to be logged (by automatic scheduling to
the right moment they are processed).

Fixes #1032 (https://opengnsys.es/trac/ticket/1032#comment:2)

admin/WebConsole/comandos/gestores/gestor_Comandos.php
@@ -257,7 +257,7 @@ if($sw_ejya=='on' || $sw_ejprg=="on" ){
 		if($sw_ejya=='on'){
 			if ($sw_seguimiento == 1) {
 				$when = now_params();
-				$resul = create_schedule(strval($sesion),
+				$resul = create_schedule_now(strval($sesion),
 							 $EJECUCION_PROCEDIMIENTO,
 							 "", $when['map_year'],
 							 $when['map_month'],

admin/WebConsole/includes/restfunctions.php
@@ -619,6 +619,30 @@ function create_schedule($task_id, $type, $name, $years, $months, $weeks,
 	return common_request(OG_REST_CMD_CREATE_SCHEDULE, POST, $data);
 }
 
+function create_schedule_now($task_id, $type, $name) {
+	$type_string;
+
+	switch ($type) {
+	case TYPE_COMMAND:
+		$type_string = OG_SCHEDULE_COMMAND;
+		break;
+	case TYPE_PROCEDURE:
+		$type_string = OG_SCHEDULE_PROCEDURE;
+		break;
+	case TYPE_TASK:
+	default:
+		$type_string = OG_SCHEDULE_TASK;
+	}
+
+	$data = array (
+		OG_REST_PARAM_TASK => $task_id,
+		OG_REST_PARAM_TYPE => $type_string,
+		OG_REST_PARAM_NAME => $name,
+	);
+
+	return common_request(OG_REST_CMD_CREATE_SCHEDULE, POST, $data);
+}
+
 function delete_schedule($schedule_id) {
 
 	$data = array (

commit ac2d1cc7e4de0632d458c69d3305c72d1d4b6156

#1032 Use create_schedule_now for tasks and procedures
Commit afbfe2a introduced create_schedule_now for commands. Adapt
procedures and task to use it instead. Drop $when variable as it is no
longer needed for "immediate scheduling" execution of commands,
procedures or tasks.

Fixes: afbfe2a (Use create_schedule_now for immediate logged commands)

admin/WebConsole/comandos/gestores/gestor_Comandos.php
@@ -256,15 +256,9 @@ if($sw_ejya=='on' || $sw_ejprg=="on" ){
 		$script=@urldecode($ValorParametros["scp"]);
 		if($sw_ejya=='on'){
 			if ($sw_seguimiento == 1) {
-				$when = now_params();
 				$resul = create_schedule_now(strval($sesion),
 							 $EJECUCION_PROCEDIMIENTO,
-							 "", $when['map_year'],
-							 $when['map_month'],
-							 0, 0, $when['map_day'],
-							 $when['map_hour'],
-							 $when['map_am_pm'],
-							 $when['map_minute']);
+							 "");
 			} else {
 				run_command($idcomando, $cadenaip, $cadenamac, $atributos);
 			}

admin/WebConsole/gestores/gestor_ejecutaracciones.php
@@ -243,12 +243,7 @@ function insertaComando($idcomando,$parametros,$idprocedimiento,$ambito,$idambit
 		      "WHERE idaccion = @sesion";
 	$resul=$cmd->Ejecutar();
 	if (resul) {
-		$when = now_params();
-		$resul = create_schedule(strval($sesion), $EJECUCION_PROCEDIMIENTO, "",
-					 $when['map_year'], $when['map_month'],
-					 0, 0, $when['map_day'],
-					 $when['map_hour'], $when['map_am_pm'],
-					 $when['map_minute']);
+		$resul = create_schedule_now(strval($sesion), $EJECUCION_PROCEDIMIENTO, "");
 	}
 	return(true);
 }