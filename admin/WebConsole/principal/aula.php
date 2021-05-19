opengnsys/OpenGnsys

commit 8634dd3ac37bc821afd0e7f321eb0064247074a2

#995 Show client link speed if available
If the clients API response included speed information, show it in the
default room view.

In case link speed is less than 1000 Mbit, highlight so.

admin/WebConsole/principal/aula.php
@@ -15,6 +15,7 @@ include_once("../clases/MenuContextual.php");
 include_once("../includes/constantes.php");
 include_once("../includes/comunes.php");
 include_once("../includes/CreaComando.php");
+include_once("../includes/restfunctions.php");
 include_once("../idiomas/php/".$idioma."/aulas_".$idioma.".php");
 include_once("../idiomas/php/".$idioma."/estados_".$idioma.".php");
 include_once("../idiomas/php/".$idioma."/comandos/mensajes_".$idioma.".php");
@@ -286,6 +287,12 @@ function pintaordenadores(){
 	global $map;
 	global $max_col;
 
+	$clients = clients_v2(2, $Mip);
+	$get_speed = function($cli) {
+		return $cli['speed'] ? $cli['speed'] : '-1';
+	};
+	$speeds = array_map($get_speed, $clients['clients']);
+
 	$ntr=0; // Numero de ordenadores por fila
 	if ($nombreaula!=""){
 		echo '<div>';
@@ -313,6 +320,13 @@ function pintaordenadores(){
 			echo '	<strong><font color="#D0A126">'.$Mip[$i].'</font></strong>';			
 			echo '	</br>';
 			echo '	<font color="#003300" size="1" face="Arial, Helvetica, sans-serif">'.$Mmac[$i].'</font>';
+			echo '	</br>';
+			if ($speeds[$i] > 0 && $speeds[$i] < 1000) {
+				echo '	<font style="background-color: red" size="1" face="Arial, Helvetica, sans-serif">'.$speeds[$i].' Mbit</font>';
+			}
+			if ($speeds[$i] > 0 && $speeds[$i] >= 1000) {
+				echo '	<font size="1" color="#007700" face="Arial, Helvetica, sans-serif">'.$speeds[$i].' Mbit</font>';
+			}
 			echo '</td>';
 			echo '</tr>';
 			echo '</table>';

admin/WebConsole/includes/restfunctions.php
@@ -77,6 +77,7 @@ define('OG_REST_PARAM_AM_PM', 'am_pm');
 define('OG_REST_PARAM_MINUTES', 'minutes');
 define('OG_REST_PARAM_MODE', 'mode');
 define('OG_REST_PARAM_SCOPE_NAME', 'scope_name');
+define('OG_REST_PARAM_SPEED', 'speed');
 
 define('TYPE_COMMAND', 1);
 define('TYPE_PROCEDURE', 2);
@@ -186,6 +187,22 @@ function clients($case, $ips) {
 	}
 
 	return $trama_notificacion;
+}
+
+function clients_v2($case, $ips) {
+
+	switch ($case) {
+		case 1:
+			$type = POST;
+			$data = array(OG_REST_PARAM_CLIENTS => $ips);
+			break;
+		case 2:
+			$type = GET;
+			$data = null;
+			break;
+	}
+
+	return common_request(OG_REST_CMD_CLIENTS, $type, $data);
 }
 
 function wol($type_wol, $ips) {

commit 29c2c73e14d90cfa803b21765c1dc6dc8a7252ad

#995 Fix client speed view inside a room
Before, speed information was not shown in the corresponding computer,
but from first to last element in the table, this did not take into
account for shutdown computers or the position inside the room for a
given computer.

This fix maps ips to speed information so it can later be indexed to
show information correctly.

Fixes: 8634dd3 (#995 Show client link speed if available)

admin/WebConsole/principal/aula.php
@@ -290,11 +290,16 @@ function pintaordenadores(){
 	global $map;
 	global $max_col;
 
-	$clients = clients_v2(2, $Mip);
-	$get_speed = function($cli) {
+	$clients = clients_v2(2, $Mip)['clients'];
+	$addr_clients = function($cli) {
+		return $cli['addr'];
+	};
+	$speed_clients = function($cli) {
 		return $cli['speed'] ? $cli['speed'] : '-1';
 	};
-	$speeds = array_map($get_speed, $clients['clients']);
+	$clients_speed = array_map($speed_clients, $clients);
+	$clients_addr = array_map($addr_clients, $clients);
+	$addr_speed_map = array_combine($clients_addr, $clients_speed);
 
 	$ntr=0; // Numero de ordenadores por fila
 	if ($nombreaula!=""){
@@ -324,11 +329,10 @@ function pintaordenadores(){
 			echo '	</br>';
 			echo '	<font color="#003300" size="1" face="Arial, Helvetica, sans-serif">'.$Mmac[$i].'</font>';
 			echo '	</br>';
-			if ($speeds[$i] > 0 && $speeds[$i] < 1000) {
-				echo '	<font style="background-color: red" size="1" face="Arial, Helvetica, sans-serif">'.$speeds[$i].' Mbit</font>';
-			}
-			if ($speeds[$i] > 0 && $speeds[$i] >= 1000) {
-				echo '	<font size="1" color="#007700" face="Arial, Helvetica, sans-serif">'.$speeds[$i].' Mbit</font>';
+			if ($addr_speed_map[$Mip[$i]] > 0 && $addr_speed_map[$Mip[$i]] >= 1000) {
+				echo '	<font size="1" color="#007700" face="Arial, Helvetica, sans-serif">'.$addr_speed_map[$Mip[$i]].' Mbit</font>';
+			} elseif ($addr_speed_map[$Mip[$i]] > 0 && $addr_speed_map[$Mip[$i]] < 1000) {
+				echo '	<font size="1" color="#990000" face="Arial, Helvetica, sans-serif">'.$addr_speed_map[$Mip[$i]].' Mbit</font>';
 			}
 			echo '</td>';
 			echo '</tr>';