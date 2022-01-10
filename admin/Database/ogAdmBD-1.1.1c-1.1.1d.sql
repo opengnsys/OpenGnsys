d0388e50eaac8f06fe744805ae9f7d1325ade07c
Irina Gómez <irinagomez@us.es>
2022-01-10 10:53:50 +0100

    #1066 #991 Updating SQL files. (equivalent to commit 2b15bdb)

---
+++ admin/Database/ogAdmBD-1.1.1c-1.1.1d.sql
@@ -0,0 +1,27 @@
+### Database update file.
+# OpenGnsys 1.1.1, 1.1.1a, 1.1.1b, 1.1.1c - OpenGnsys 1.1.1d
+#use ogAdmBD
+
+DROP PROCEDURE IF EXISTS altercols;
+# Procedure to perform conditional table update.
+DELIMITER '//'
+CREATE PROCEDURE altercols() BEGIN
+	# Add maintenance and remote access fields for computers (tickets #991 y #992).
+	IF NOT EXISTS (SELECT * FROM information_schema.COLUMNS
+			WHERE COLUMN_NAME='inremotepc' AND TABLE_NAME='ordenadores' AND TABLE_SCHEMA=DATABASE())
+	THEN
+	ALTER TABLE ordenadores
+			ADD inremotepc SMALLINT NOT NULL DEFAULT 0,
+			ADD maintenance SMALLINT NOT NULL DEFAULT 0;
+	END IF;
+END//
+# Run conditional update.
+DELIMITER ';'
+CALL altercols();
+DROP PROCEDURE altercols;
+
+# Redefine some fields as not null.
+ALTER TABLE aulas
+      MODIFY inremotepc SMALLINT NOT NULL DEFAULT 0;
+ALTER TABLE imagenes
+      MODIFY inremotepc SMALLINT NOT NULL DEFAULT 0;
